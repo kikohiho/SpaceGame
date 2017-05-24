5e4891a60fea90c944fa0239df756e445493167f
kikohiho <ricardo.vila1@upr.edu>
2017-05-24 17:17:08 -0400

    Mock Main Menu

---
+++ Assets/Shipcontroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shipcontroller : MonoBehaviour {
+
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical);
+        transform.rotation = Quaternion.Euler(new Vector3(0, moveHorizontal, 0));
+
+        rb.AddForce(movement * speed);
+        
+    }
+}
