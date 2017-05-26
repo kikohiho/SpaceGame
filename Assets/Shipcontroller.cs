1ab7bc1aca375cb3d52d6e03cf6ad6a28b091b4e
rafael001 <rafael.marrero4@upr.edu>
2017-05-25 22:07:00 -0400

    Modifique mock ship y el ship controller

--- Assets/Shipcontroller.cs
+++ Assets/Shipcontroller.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Shipcontroller : MonoBehaviour {
 
-
+    // Puse esta parte comentado por si las moscas
+    /*
     public float speed;
 
     private Rigidbody rb;
@@ -25,10 +26,52 @@ public class Shipcontroller : MonoBehaviour {
         rb.AddForce(movement * speed);
 
         
-    }
+    }*/
+    
 
-    void Slowdown()
+    public float speed;
+    public float turnSpeed;
+    public float tilt;
+    // Tilt se usara eventualmente para que la nave haga un roll.
+
+    private float moveHorizontal;
+    private float moveVertical;
+    private float turn;
+
+    public Rigidbody rb;
+
+    private void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
+
+    private void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Turn();
+    }
+
+    // La idea para las funciones Move y Turn lo saque
+    // del Tank tutorial de Unity
+    private void Move()
+    {
+        // Movimiento hacia al frente de la nave
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(transform.forward * speed);
+    }
+
+    private void Turn()
+    {
+        // Rotar la nave en el eje y.
+        turn = moveHorizontal * turnSpeed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0.0f, turn, 0.0f);
+        rb.MoveRotation(rb.rotation * turnRotation);
+    }
+
 }
